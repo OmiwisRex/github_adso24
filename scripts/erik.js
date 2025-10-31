a20d5910c5f45cf9e9e78e476b3bd9f6b6bde4f2
ErikSantiagoCastellanos <chilamarae@gmail.com>
2025-10-31 14:04:46 -0500

    Rename function eriksantiago to erik

--- scripts/erik.js
+++ scripts/erik.js
@@ -1,6 +1,6 @@
-function eriksantiago(a, b) {
+function erik(a, b) {
   let suma = a + b;
   return `La suma de ${a} y ${b} es ${suma}`;
 }
 
-console.log(eriksantiago(5, 8));
+console.log(erik(5, 8));
